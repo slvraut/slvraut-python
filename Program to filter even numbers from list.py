55847ebdb4cb7425f6293fc70119314db5ae593d
slvraut <110077896+slvraut@users.noreply.github.com>
2022-07-27 09:27:55 +0545

    Update Program to filter even numbers from list.py

--- Program to filter even numbers from list.py
+++ Program to filter even numbers from list.py
@@ -1,6 +1,6 @@
 #Program to filter even numbers from list#
 
-list1=[i for i in range(51)]#list of numbers from 1 to 50
+list1=[i for i in range(61)]#list of numbers from 1 to 61
 #print(list1)
 evenList=[x for x in list1 if x>1 and x%2==0]#condition to select even numbers from the list
 print(evenList)
