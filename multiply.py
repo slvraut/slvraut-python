614b76000ec6cc13cf9b8ec091eeefae63d483ba
slvraut <110077896+slvraut@users.noreply.github.com>
2022-08-02 09:29:33 +0545

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,12 @@
+# Python3 program to multiply all values in the
+# list using math.prod
+
+import math
+list1 = [1, 2, 3]
+list2 = [3, 2, 4]
+
+
+result1 = math.prod(list1)
+result2 = math.prod(list2)
+print(result1)
+print(result2)
